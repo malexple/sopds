929cde1c4c5bc0dc2d547725eb3c557b655f51e8
malexple <malexple@gmail.com>
2026-06-12 12:01:44 +0300

    fix read book

--- src/main/java/com/sopds/config/SecurityConfig.java
+++ src/main/java/com/sopds/config/SecurityConfig.java
@@ -20,6 +20,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .headers(h -> h.frameOptions(f -> f.sameOrigin()))
                 .authorizeHttpRequests(auth -> auth
                         // Swagger / OpenAPI — ВСЕ пути
                         .requestMatchers(
